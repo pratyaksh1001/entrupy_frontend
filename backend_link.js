821671454eb6ade06518a03e5e3ddde36b64e141
pratyksh1001 <pratyakshkarmahe@gmail.com>
2026-04-02 07:26:06 +0530

    handled CORS errors

--- backend_link.js
+++ backend_link.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    // Use Next.js rewrite proxy by default to avoid browser CORS issues.
-    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://127.0.0.1:8000/",
+    baseURL: process.env.BACK_END || "http://127.0.0.1:8000/",
     headers: {
         "Content-Type": "application/json",
     },
